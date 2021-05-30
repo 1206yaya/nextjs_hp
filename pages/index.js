818088622a85b0e899e90558aa7c754fedafad5a
1206yaya <1206yaya@gmail.com>
2021-05-30 14:50:26 +0900

    edit index.js

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import Layout from '../components/Layout';
 export default function Home() {
   return (
     <Layout title="home">
-      <p className="text-4xl">Welcome to Nextjs</p>
+      <p className="text-4xl">Welcome to Nextjs!!!</p>
     </Layout>
   );
 }
